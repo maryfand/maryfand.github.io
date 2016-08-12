38876be336512b11498d89c32b1497968692ef87
maryfand <547284607@qq.com>
2016-08-12 21:44:46 +0800

    fhh

--- src/components/Home/Works.js
+++ src/components/Home/Works.js
@@ -16,42 +16,42 @@ class Works extends React.Component {
               <img src="http://obbjyirz0.bkt.clouddn.com/668573_210906041541_2~001.jpg" />
             </CardMedia>
           </Card>
-          </a>
-          <a href="https://maryfand.github.io/blog" className="work-list">
+        </a>
+        <a href="https://maryfand.github.io/blog" className="work-list">
             <Card className="work-card">
               <CardMedia overlay={<CardTitle title="文档手册" subtitle="我的个人博客" style={styles.title}/>} >
                 <img src="http://obbjyirz0.bkt.clouddn.com/668573_210906041541_2~001.jpg" />
               </CardMedia>
             </Card>
-          </a>
-            <a href="https://maryfand.github.io/html5" className="work-list">
+        </a>
+        <a href="https://maryfand.github.io/html5" className="work-list">
             <Card className="work-card">
               <CardMedia overlay={<CardTitle title="html5" subtitle=" 小 demo" style={styles.title}/>} >
                 <img src="http://obbjyirz0.bkt.clouddn.com/668573_210906041541_2~001.jpg" />
               </CardMedia>
             </Card>
-            </a>
-            <a href="https://maryfand.github.io/Some-exercises/" className="work-list">
+        </a>
+        <a href="https://maryfand.github.io/Some-exercises/" className="work-list">
               <Card className="work-card">
                 <CardMedia overlay={<CardTitle title="其他案例" subtitle="一些练手作品" style={styles.title}/>} >
                   <img src="http://obbjyirz0.bkt.clouddn.com/%E6%B7%B1%E5%BA%A6%E6%88%AA%E5%9B%BE20160812205910.png" />
                 </CardMedia>
               </Card>
-            </a>
-            <a href="https://maryfand.github.io/100" className="work-list">
+        </a>
+        <a href="https://maryfand.github.io/100" className="work-list">
               <Card className="work-card">
                 <CardMedia overlay={<CardTitle title="react" subtitle="作品集" style={styles.title}/>} >
                   <img src="http://obbjyirz0.bkt.clouddn.com/668573_210906041541_2~001.jpg" />
                 </CardMedia>
               </Card>
-            </a>
-            <a href="https://maryfand.github.io/100" className="work-list">
+        </a>
+        <a href="https://maryfand.github.io/100" className="work-list">
               <Card className="work-card">
                 <CardMedia overlay={<CardTitle title="javascript" subtitle="我的小案例" style={styles.title}/>} >
                   <img src="http://obbjyirz0.bkt.clouddn.com/668573_210906041541_2~001.jpg" />
                 </CardMedia>
               </Card>
-            </a>
+        </a>
       </div>
     )
   }
